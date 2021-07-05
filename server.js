c9c3e3bb6b39381e6c3cfa7477097a858ad6dc71
Avijit <adavijit@yahoo.com>
2021-07-05 13:26:12 +0530

    updated

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const multer = require('multer');
-const path = require('path');
+
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
